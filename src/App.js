fb15a868bd8e9886f023c2765ea804d994c4f2c1
Diana T <di.tamas.dt@gmail.com>
2018-12-06 08:28:12 +0000

    first version of venue cards and pagination

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import VenueCard from './VenueCard'
+
+const rowsPerPage = 10
 
 class App extends Component {
+
+  state = {
+    venues: [],
+    page: 0
+  }
+
+  componentDidMount () {
+      fetch('https://venue-lister.herokuapp.com/venues')
+      .then(data => data.json())
+      .then(venues => this.setState({ venues }))
+  }
+
+  handleChangePage = (page) => {
+    this.setState({ page })
+  }
+
   render() {
+    const { page, venues } = this.state
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid container spacing={ 16 }>
+        {venues.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(venue =>
+          <Grid item xs={ 3 } key={ venue.id }>
+            <VenueCard venue={ venue } />
+          </Grid>
+        )}
+        </Grid>
+        <Button variant='contained' onClick={ () => this.handleChangePage(page + 1) }>Next</Button>
+        <Button variant='contained' onClick={ () => this.handleChangePage(page - 1) }>Previous</Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/VenueCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+class VenueCard extends Component {
+
+  render ()  {
+    const { venue } = this.props
+
+    return (
+      <Card raised style={{ height: 250 }}>
+        <CardHeader title={ venue.name } subheader={ venue.address1 + ', ' + venue.city } />
+        <CardContent>
+          <Typography>{ venue.listing_text }</Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default VenueCard
